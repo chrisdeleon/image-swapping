0a275f7fa4c181067ce6de44f5b2f84c57704986
Chris de Leon <95385991+chrisdeleon@users.noreply.github.com>
2022-08-29 16:07:48 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function index(Number){
+	if(Number == 1){
+		document.getElementById('main').src="first-image.jpg";
+	} else if(Number == 2){
+		document.getElementById('main').src="second-image.jpg";
+	} else if(Number == 3){
+		document.getElementById('main').src="third-image.jpg";
+	} else if(Number == 4){
+		document.getElementById('main').src="fourth-image.jpg";
+	}
+}
